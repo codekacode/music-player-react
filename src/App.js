f986d89207c20821d78a4f0d380ca210cb209b56
Codekacode <karem.quiroz.m@gmail.com>
2021-04-23 22:46:15 -0500

    Add state and handler to change audio and time

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Player from './components/Player';
 import Song from './components/Song';
 import Library from './components/Library';
 import Nav from './components/Nav';
-import data from './util'
+import data from './data'
 
 function App() {
   //state
@@ -12,13 +12,32 @@ function App() {
   const [currentSong, setCurrentSong] = useState(songs[0]);
   const [isPlaying, setIsPlaying] = useState(false);
   const [libraryStatus, setLibraryStatus] = useState(false);
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    durationTime: 0
+  })
   const audioRef = useRef(null);
+
+  const timeSongHandle = (e) => {
+// with the event we can extract duration
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({...songInfo, currentTime: current, durationTime: duration})
+  }
+
+  const songEndHandler = async() => {
+    let currentIndex = songs.findIndex(song => song.id === currentSong.id);
+    await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+    if(isPlaying) audioRef.current.play();
+  }
+
   return (
     <div className="App">
       <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus}/>
       <Song currentSong={currentSong}/>
-      <Player setIsPlaying={setIsPlaying} isPlaying={isPlaying} currentSong={currentSong} audioRef={audioRef}/>
+      <Player setSongInfo={setSongInfo} songInfo={songInfo} setIsPlaying={setIsPlaying} isPlaying={isPlaying} currentSong={currentSong} audioRef={audioRef} setCurrentSong={setCurrentSong} setSongs={setSongs} songs={songs} />
       <Library songs={songs} setCurrentSong={setCurrentSong} isPlaying={isPlaying} audioRef={audioRef} setSongs={setSongs} libraryStatus={libraryStatus}/>
+      <audio onLoadedMetadata={timeSongHandle} onTimeUpdate={timeSongHandle} ref={audioRef} src={currentSong.audio} onEnded={songEndHandler}></audio>
     </div>
   );
 }
